1f12f785423e7a2e3a47f16d8c80c07f44a832f6
wxl <wangxianliang@yunrongtech.com>
2017-04-17 18:10:35 +0800

    j

---
+++ windows/qga/main.d
@@ -0,0 +1,345 @@
+qga/main.o: qga/main.c \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/galloca.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gtypes.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/lib/glib-2.0/include/glibconfig.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gmacros.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gversionmacros.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/garray.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gasyncqueue.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gthread.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gatomic.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gerror.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gquark.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gbacktrace.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gbase64.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gbitlock.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gbookmarkfile.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gbytes.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gcharset.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gchecksum.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gconvert.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gdataset.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gdate.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gdatetime.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gtimezone.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gdir.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/genviron.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gfileutils.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/ggettext.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/ghash.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/glist.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gmem.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gnode.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/ghmac.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gchecksum.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/ghook.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/ghostutils.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/giochannel.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gmain.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gpoll.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gslist.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gstring.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gunicode.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gutils.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gkeyfile.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gmappedfile.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gmarkup.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gmessages.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/goption.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gpattern.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gprimes.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gqsort.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gqueue.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/grand.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gregex.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gscanner.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gsequence.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gshell.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gslice.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gspawn.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gstrfuncs.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gstringchunk.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gtestutils.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gthreadpool.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gtimer.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gtrashstack.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gtree.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gurifuncs.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gvarianttype.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gvariant.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gversion.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gwin32.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/deprecated/gallocator.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/deprecated/gcache.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/deprecated/gcompletion.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/deprecated/gmain.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/deprecated/grel.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/deprecated/gthread.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gstdio.h \
+ /usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gprintf.h \
+ /home/wxl/git/qemu-ga-win/include/qapi/qmp/json-streamer.h \
+ /home/wxl/git/qemu-ga-win/include/glib-compat.h \
+ /home/wxl/git/qemu-ga-win/include/qapi/qmp/json-lexer.h \
+ /home/wxl/git/qemu-ga-win/include/qapi/qmp/json-parser.h \
+ /home/wxl/git/qemu-ga-win/include/qemu-common.h \
+ /home/wxl/git/qemu-ga-win/include/qemu/osdep.h config-host.h \
+ /home/wxl/git/qemu-ga-win/include/qemu/compiler.h \
+ /home/wxl/git/qemu-ga-win/include/sysemu/os-win32.h \
+ /home/wxl/git/qemu-ga-win/include/qapi/error.h qapi-types.h \
+ /home/wxl/git/qemu-ga-win/include/qapi/qmp/qobject.h \
+ /home/wxl/git/qemu-ga-win/include/qemu/typedefs.h \
+ /home/wxl/git/qemu-ga-win/include/qemu/fprintf-fn.h \
+ /home/wxl/git/qemu-ga-win/include/glib-compat.h \
+ /home/wxl/git/qemu-ga-win/include/qemu/option.h \
+ /home/wxl/git/qemu-ga-win/include/qemu/queue.h \
+ /home/wxl/git/qemu-ga-win/include/qemu/atomic.h \
+ /home/wxl/git/qemu-ga-win/include/qapi/qmp/qdict.h \
+ /home/wxl/git/qemu-ga-win/include/qapi/qmp/qlist.h \
+ /home/wxl/git/qemu-ga-win/include/qemu/typedefs.h \
+ /home/wxl/git/qemu-ga-win/include/qemu/host-utils.h \
+ /home/wxl/git/qemu-ga-win/include/qemu/bswap.h \
+ /home/wxl/git/qemu-ga-win/include/fpu/softfloat.h \
+ /home/wxl/git/qemu-ga-win/include/qemu/osdep.h \
+ /home/wxl/git/qemu-ga-win/include/qemu/bswap.h \
+ /home/wxl/git/qemu-ga-win/include/qemu/module.h \
+ /home/wxl/git/qemu-ga-win/include/qapi/qmp/qint.h \
+ /home/wxl/git/qemu-ga-win/include/qapi/qmp/qjson.h \
+ /home/wxl/git/qemu-ga-win/include/qapi/qmp/qstring.h \
+ qga/guest-agent-core.h \
+ /home/wxl/git/qemu-ga-win/include/qapi/qmp/dispatch.h \
+ /home/wxl/git/qemu-ga-win/include/qemu/module.h \
+ /home/wxl/git/qemu-ga-win/include/qapi/qmp/qerror.h qga/channel.h \
+ qga/service-win32.h qga/vss-win32.h
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/galloca.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gtypes.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/lib/glib-2.0/include/glibconfig.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gmacros.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gversionmacros.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/garray.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gasyncqueue.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gthread.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gatomic.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gerror.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gquark.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gbacktrace.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gbase64.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gbitlock.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gbookmarkfile.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gbytes.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gcharset.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gchecksum.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gconvert.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gdataset.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gdate.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gdatetime.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gtimezone.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gdir.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/genviron.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gfileutils.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/ggettext.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/ghash.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/glist.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gmem.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gnode.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/ghmac.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gchecksum.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/ghook.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/ghostutils.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/giochannel.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gmain.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gpoll.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gslist.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gstring.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gunicode.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gutils.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gkeyfile.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gmappedfile.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gmarkup.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gmessages.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/goption.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gpattern.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gprimes.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gqsort.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gqueue.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/grand.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gregex.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gscanner.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gsequence.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gshell.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gslice.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gspawn.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gstrfuncs.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gstringchunk.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gtestutils.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gthreadpool.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gtimer.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gtrashstack.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gtree.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gurifuncs.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gvarianttype.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gvariant.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gversion.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gwin32.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/deprecated/gallocator.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/deprecated/gcache.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/deprecated/gcompletion.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/deprecated/gmain.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/deprecated/grel.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/deprecated/gthread.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gstdio.h:
+
+/usr/x86_64-w64-mingw32/sys-root/mingw/include/glib-2.0/glib/gprintf.h:
+
+/home/wxl/git/qemu-ga-win/include/qapi/qmp/json-streamer.h:
+
+/home/wxl/git/qemu-ga-win/include/glib-compat.h:
+
+/home/wxl/git/qemu-ga-win/include/qapi/qmp/json-lexer.h:
+
+/home/wxl/git/qemu-ga-win/include/qapi/qmp/json-parser.h:
+
+/home/wxl/git/qemu-ga-win/include/qemu-common.h:
+
+/home/wxl/git/qemu-ga-win/include/qemu/osdep.h:
+
+config-host.h:
+
+/home/wxl/git/qemu-ga-win/include/qemu/compiler.h:
+
+/home/wxl/git/qemu-ga-win/include/sysemu/os-win32.h:
+
+/home/wxl/git/qemu-ga-win/include/qapi/error.h:
+
+qapi-types.h:
+
+/home/wxl/git/qemu-ga-win/include/qapi/qmp/qobject.h:
+
+/home/wxl/git/qemu-ga-win/include/qemu/typedefs.h:
+
+/home/wxl/git/qemu-ga-win/include/qemu/fprintf-fn.h:
+
+/home/wxl/git/qemu-ga-win/include/glib-compat.h:
+
+/home/wxl/git/qemu-ga-win/include/qemu/option.h:
+
+/home/wxl/git/qemu-ga-win/include/qemu/queue.h:
+
+/home/wxl/git/qemu-ga-win/include/qemu/atomic.h:
+
+/home/wxl/git/qemu-ga-win/include/qapi/qmp/qdict.h:
+
+/home/wxl/git/qemu-ga-win/include/qapi/qmp/qlist.h:
+
+/home/wxl/git/qemu-ga-win/include/qemu/typedefs.h:
+
+/home/wxl/git/qemu-ga-win/include/qemu/host-utils.h:
+
+/home/wxl/git/qemu-ga-win/include/qemu/bswap.h:
+
+/home/wxl/git/qemu-ga-win/include/fpu/softfloat.h:
+
+/home/wxl/git/qemu-ga-win/include/qemu/osdep.h:
+
+/home/wxl/git/qemu-ga-win/include/qemu/bswap.h:
+
+/home/wxl/git/qemu-ga-win/include/qemu/module.h:
+
+/home/wxl/git/qemu-ga-win/include/qapi/qmp/qint.h:
+
+/home/wxl/git/qemu-ga-win/include/qapi/qmp/qjson.h:
+
+/home/wxl/git/qemu-ga-win/include/qapi/qmp/qstring.h:
+
+qga/guest-agent-core.h:
+
+/home/wxl/git/qemu-ga-win/include/qapi/qmp/dispatch.h:
+
+/home/wxl/git/qemu-ga-win/include/qemu/module.h:
+
+/home/wxl/git/qemu-ga-win/include/qapi/qmp/qerror.h:
+
+qga/channel.h:
+
+qga/service-win32.h:
+
+qga/vss-win32.h:
